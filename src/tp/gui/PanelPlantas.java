02541ecab1ce29e693c19db9d6a42e491b4fc140
JU4N98 <juanpablocabana2@gmail.com>
2020-07-31 00:28:39 -0300

    Vuelvo a poner boton eliminar en planta.

--- src/tp/gui/PanelPlantas.java
+++ src/tp/gui/PanelPlantas.java
@@ -175,7 +175,7 @@ public class PanelPlantas extends PanelPersonalizado{
 		panel1.setBorder(borde1);
 		
 		colocar(0,0,2,1,1,1,0,0,GridBagConstraints.BOTH, 10, panel1, scroll_pane);
-		colocar(0,1,2,1,0,0,0,0,GridBagConstraints.NONE,10,panel1,boton_stock_insumo);
+		colocar(1,1,2,1,0,0,0,0,GridBagConstraints.NONE,10,panel1,boton_eliminar);
 		
 	//PANEL2------------------------------------------------------------------------------------------------
 		JPanel panel2 = new JPanel();
