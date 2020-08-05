1f67091029464589599b484f8f628f12d775a94d
fideldalmasso <fidelmasso@gmail.com>
2020-08-05 18:15:13 -0300

    Empieza con el poblado

--- scriptPobladoDeTablas.sql
+++ scriptPobladoDeTablas.sql
@@ -4,47 +4,88 @@ insert into tp.marca values
 	('Renault'),
 	('Volvo'),
 	('Man'),
-	('Daf'),
-	('Mercedes-benz'),
-	('Nissan'),
-	('Isuzu'),
-	('Mitsubishi');
+	('Daf');
 
 insert into tp.modelo values
-('modelo1','Scania'),
-('modelo2','Iveco'),
-('modelo3','Renault'),
-('modelo4','Volvo'),
-('modelo5','Man'),
-('modelo6','Daf');
+('K250IB','Scania'),
+('N230UB','Scania'),
+('N270UB','Scania'),
+
+('Tector Attack','Iveco'),
+('Tector Evo','Iveco'),
+('Stralis','Iveco'),
+
+('T High 440','Renault'),
+('T High 520','Renault'),
+('D 280','Renault'),
+
+('FMX','Volvo'),
+('VM','Volvo'),
+('FH','Volvo'),
+
+('TGX D38','Man'),
+('L 2000','Man'),
+('TGM','Man'),
+
+('XF Space','Daf'),
+('XF95 Tropco','Daf'),
+('XF 95.480','Daf');
 
 
 insert into tp.planta values
-(1, 'Planta1'),
-(2, 'Planta2'),
-(3, 'Planta3'),
-(4, 'Planta4'),
-(5, 'Planta5');
+(default, 'COFCO'),
+(default, 'AGD'),
+(default, 'Vicentín'),
+(default, 'A.D.M. Agro'),
+(default, 'Cargill'),
+(default, 'Bunge'),
+(default, 'Oleaginosa Moreno'),
+(default, 'Amaggi'),
+(default, 'Cargill'),
+(default, 'ACA');
 
 insert into tp.ruta values
-('r1',1,2,200,60,1000),
-('r2',1,3,50,120,3000),
-('r3',1,4,150,60,1000),
-('r4',2,5,200,60,1000),
-('r5',3,5,200,120,1000),
-('r6',3,4,50,120,2000),
-('r7',4,5,50,60,3000);
-
+('RN01',4,5,50  ,37.5,598517.6),
+('RN03',2,5,3060,2295,56705.7),
+('RN05',4,5,545 ,408.75,783060.9),
+('RN07',4,5,1224,918,245206.4),
+('RN08',4,5,695 ,521.25,385843.4),
+('RN09',3,4,1967,1475.2,922756.2),
+('RN11',3,4,980 ,735,767173.0),
+('RN12',3,4,1560,1170,492579.7),
+('RN14',3,4,1127,845.25,693260.6),
+('RN17',1,4,337 ,252.75,561901.0),
+('RN18',1,4,227 ,170.25,853246.3),
+('RN20',1,4,582 ,436.5,339037.1),
+('RN22',3,5,685 ,513.75,126983.2),
+('RN38',1,3,807 ,605.25,48850.1),
+('RN40',1,2,5224,3918,842250.1);
 
 insert into tp.camion values
-('IXM242',1,'modelo1',0.0,400,1000,'2017-01-10'),
-('NIM094',2,'modelo2',0.0,400,1000,'2020-01-17');
+('IXM242',1,'K250IB',0.0,400,1000,'2017-01-10'),
+('NIM094',2,'N230UB',0.0,400,1000,'2020-01-17');
 
-insert into tp.insumo values 
-(1,'Arena','KILO',100);
+insert into tp.Insumo values 
+(default,'Arena','KILO',670),
+(default,'Agua','LITRO',500),
+(default,'Aceite','METRO_CUBICO',30),
+(default,'Soja','KILO',146),
+(default,'Fertilizantes','GRAMO',34),
+(default,'Desinfectante','LITRO',339),
+(default,'Lavandina','PIEZA',16),
+(default,'Caucho','METRO_CUADRADO',980);
 
-insert into tp.insumoLiquido values 
-(1,300);
+insert into tp.InsumoGeneral values 
+(1,500),
+(4,90),
+(5,307),
+(8,900);
+
+insert into tp.InsumoLiquido values 
+(2,1),
+(3, 0.8),
+(6, 2.5),
+(7, 1.2);
 
 insert into tp.stockinsumo values
 (1,1,100,20);
